857ee57ab6d082621d408f572278c3e0264b7085
Alina <AliKochneva@yandex.ru>
2022-10-23 20:17:28 +0700

    Seminar 7

--- Seminar_7/Ex_02/Program.cs
+++ Seminar_7/Ex_02/Program.cs
@@ -1,2 +1,36 @@
 ﻿/* Написать программу, которая в двумерном массиве заменяет строки на столбцы
  или сообщить, что это невозможно (в случае, если матрица не квадратная)*/
+
+
+ int min = new Random().Next(1, 4);
+int max = new Random().Next(5, 9);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+}
+
+int[,] matrix = new int[3, 5];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
--- Seminar_7/Ex_03/Program.cs
+++ Seminar_7/Ex_03/Program.cs
@@ -1,1 +1,34 @@
 ﻿// В прямоугольной матрице найти строку с наименьшей суммой элементов
+
+int min = new Random().Next(1, 4);
+int max = new Random().Next(5, 9);
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($" {matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+}
+
+int[,] matrix = new int[3, 5];
+
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
