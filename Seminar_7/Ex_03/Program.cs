fa77b1b14383190cb4800d754182a89c82d21ab2
Alina <AliKochneva@yandex.ru>
2022-10-25 13:19:51 +0700

    В прямоугольной матрице найти строку с наименьшей суммой элементов

--- Seminar_7/Ex_03/Program.cs
+++ Seminar_7/Ex_03/Program.cs
@@ -27,8 +27,30 @@ void FillArray(int[,] matr)
     }
 }
 
+void MinimalIndex(int[,] array)
+{
+    int min = int.MaxValue;
+    int count = 0;
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum +=array[i, j];
+        }    
+    if (min > sum)
+    {
+        min = sum;
+        count = i;
+    }
+    sum = 0;
+    }
+    Console.Write($"Наименьшая сумма элиментов ({min}) находится на строке № {count + 1}");
+}
+
 int[,] matrix = new int[3, 5];
 
 FillArray(matrix);
 PrintArray(matrix);
 Console.WriteLine();
+MinimalIndex(matrix);
