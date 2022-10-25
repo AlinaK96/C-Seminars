a1c6cebbf2995a4061d0d8d3bb33de94edc04d65
Alina <AliKochneva@yandex.ru>
2022-10-25 13:52:12 +0700

    Упорядочить по убыванию элементы каждой строки двумерной массива

--- Seminar_7/Ex_01/Program.cs
+++ Seminar_7/Ex_01/Program.cs
@@ -27,8 +27,32 @@ void FillArray(int[,] matr)
     }
 }
 
+void SortArray (int[,] matr, int rowNumber)
+{
+    int change;
+    for ( int i = 0; i < matr.GetLength(1); i++)
+    {
+        for (int j = 1; j < matr.GetLength(1) - i; j++)
+        {
+            if (matr[rowNumber, j - 1] < matr[rowNumber, j])
+            {
+                change = matr[rowNumber, j];
+                matr[rowNumber, j] = matr[rowNumber, j - 1];
+                matr[rowNumber, j - 1] = change;
+            }
+        }
+    }
+}
+
 int[,] matrix = new int[3, 5];
 
 FillArray(matrix);
 PrintArray(matrix);
 Console.WriteLine();
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    SortArray(matrix, i);
+}
+Console.WriteLine();
+PrintArray(matrix);
