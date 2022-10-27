40bee9049e5cf33c8ee3b6f1053e8a04f07a4aa8
Alina <AliKochneva@yandex.ru>
2022-10-27 10:54:25 +0700

    Треугольник Паскаля

--- Seminar_8/Ex_04/Program.cs
+++ Seminar_8/Ex_04/Program.cs
@@ -1,2 +1,35 @@
 ﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
-//2.****** Показать треугольник паскаля и сделать вывод в виде равнобедрнного треугольника (40:13)
+
+int Fuctorial (int n)
+{
+    int f = 1;
+    for(int i = 1; i <=n; i++)
+    {
+        f *= i;
+    }
+    return f;
+}
+
+void Print(int n)
+{
+    for ( int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= (n-i); j++)
+        {
+            Console.Write("  ");
+        }
+        for (int j = 0; j <=i; j++)
+        {
+            Console.Write("  ");
+            Console.Write(Fuctorial(i) / (Fuctorial(j)*Fuctorial(i-j)));
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Количество строк: ");
+int rows = int.Parse(Console.ReadLine()?? "0");
+Print(rows);
+
