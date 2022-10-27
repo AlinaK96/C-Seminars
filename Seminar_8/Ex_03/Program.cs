d50673b9298097c4928db4ff8f7dfbf385b13c2f
Alina <AliKochneva@yandex.ru>
2022-10-27 12:31:31 +0700

    Трёхмерный массив с выводом индексов

--- Seminar_8/Ex_03/Program.cs
+++ Seminar_8/Ex_03/Program.cs
@@ -1,9 +1,46 @@
 ﻿/*Сформировать трехмерный массив не повторяющимися двузначными числами показать 
-его построчно на экран выводя индексы соответствующего элемента
-3.1 ***** Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+его построчно на экран выводя индексы соответствующего элемента*/
 
-массив размером 2 x 2 x 2
 
-12(0,0,0) 22(0,0,1)
 
-45(1,0,0) 53(1,0,1)*/
+void FillArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int z = 0; z < matr.GetLength(2); z++)
+            {
+                matr[i, j, z] = new Random().Next(10, 50);
+            } 
+        }
+    }
+}
+
+void PrintArray(int[,,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int z = 0; z < matr.GetLength(2); z++)
+            {
+                Console.Write($"({i},{j},{z}) = {matr[i, j, z]}   ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Write("Количество строк: ");
+int m = int.Parse(Console.ReadLine()??"0");
+Console.Write("Количество столбцов: ");
+int n = int.Parse(Console.ReadLine()??"0");
+Console.Write("Bведите глубину: ");
+int p = int.Parse(Console.ReadLine()??"0");
+int[,,] matrix = new int[m, n, p];
+Console.WriteLine("Трехмерный массив: ");
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
