ff3850f5e70b408b71563adc2c0c5968e8a1127c
Alina <AliKochneva@yandex.ru>
2022-11-12 19:52:41 +0700

    Показать натуральные числа от M до N

--- Final/Ex_01/Program.cs
+++ Final/Ex_01/Program.cs
@@ -1,1 +1,16 @@
-﻿// 
+﻿// Показать натуральные числа от M до N
+
+
+Console.Write("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine()?? "0");
+
+string PrintArray (int start, int end)
+{
+    if(start == end) return start.ToString();
+    return (start + " " + PrintArray(start+1, end));
+}
+
+
+Console.Write(PrintArray(numberM, numberN));
