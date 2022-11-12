d83753e4d2321d4da41126d811c5fb4d44e6a5f3
Alina <AliKochneva@yandex.ru>
2022-11-12 19:57:04 +0700

    Найти сумму элементов от M до N

--- Final/Ex_02/Program.cs
+++ Final/Ex_02/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+
+Console.Write("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine()?? "0");
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine()?? "0");
+
+int Sum (int start, int end)
+{
+    if (start == end) return start;
+    return (start + Sum(start + 1, end));
+}
+
+Console.Write($"Сумма чисел от {numberM} до {numberN} равна {Sum(numberM, numberN)}");
